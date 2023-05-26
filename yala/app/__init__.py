19e7274dfe13698a39ecbac8ecdd2dba15ca2941
yala <hyeonsori@163.com>
2023-05-26 10:21:28 +0800

    0526

--- yala/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from config import Config
-
-yala = Flask(__name__)
-yala.config.from_object(Config)
--- yala/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config():
-    ROOT = os.path.dirname(os.path.abspath(__file__))
-    LOG_NAME = os.path.join(ROOT, 'logs', 'yala.log')
